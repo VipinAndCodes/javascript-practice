97a15171879c1a654e4913beed73b340d4768a5a
vipin4948 <vipinkumarcp4@gmail.com>
2021-11-05 23:05:56 +0530

    starting iterators on js

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -2,14 +2,21 @@
 
 let firstCard = 10
 let secondCard = 18
+let cards = [firstCard,secondCard]
 let hashBlackJack = false
 let isAlive = true
 let message = " "
 let sum = firstCard + secondCard
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
-messageEl = document.getElementById("message-el")
 
-function startGame(){
+let cardsEL = document.getElementById("cards-el")
+
+function renderGame(){
+
+    cardsEL.textContent = "Cards :" + cards[0]+ " " + cards[1]
+    sumEl.textContent ="Sum :" + sum
 
     if (sum <= 20) {
     message = "Do you want to draw a new card"}
@@ -22,4 +29,15 @@ function startGame(){
     messageEl.textContent = message
 
 }
+function startGame() {
 
+    renderGame()
+}
+
+function newCard() {
+    console.log("Drawing a new card from deck")
+    let card = 3
+    cards.push(card)
+    renderGame()
+    
+}
